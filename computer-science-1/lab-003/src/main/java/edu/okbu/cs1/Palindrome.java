e0af14b4aff82f238eae320413e6bdf68de7b358
avaryDivine <avary.divine24@okbu.edu>
2025-03-11 12:29:55 -0500

    3-11-25

--- computer-science-1/lab-003/src/main/java/edu/okbu/cs1/Palindrome.java
+++ computer-science-1/lab-003/src/main/java/edu/okbu/cs1/Palindrome.java
@@ -1,6 +1,5 @@
 package edu.okbu.cs1;
 
-
 public class Palindrome {
     String theString;
 
@@ -9,6 +8,20 @@ public class Palindrome {
     }
 
     public boolean isPalindrome() {
-        
+        int length = theString.length();
+        boolean palindrome = true;
+        for (int i = 0; i < (length / 2); ++i) {
+            if (theString.charAt(i) != theString.charAt(length - i - 1)) {
+                palindrome = false;
+                break;
+            }
+        }
+        System.out.println(palindrome);
+        return palindrome;
+    }
+
+    public static void main(String[] args) {
+        Palindrome p = new Palindrome("racecar");
+        p.isPalindrome();
     }
 }
