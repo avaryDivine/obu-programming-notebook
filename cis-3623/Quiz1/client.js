fe3da1165e8f15ffcc530ca32ca08cb158385880
avaryDivine <avary.divine24@okbu.edu>
2025-10-29 21:15:12 -0500

    Quiz 2 finish

--- cis-3623/Quiz1/client.js
+++ cis-3623/Quiz1/client.js
@@ -15,8 +15,26 @@ async function return_SmallEvent(theSection, theEvent, theElementIdx) {
     });
     let the_data = await response.json();
     console.log(the_data); 
-}
-return_SmallEvent("the-weekend", "bison-day", 0);
+};
+//return_SmallEvent("the-weekend", "bison-day", 0);
+
+async function return_subEvent(theSection, theEvent) {
+    let response = await fetch("http://localhost:3000/return-subEvent", {
+        method: "POST",
+        headers: {
+            'Accept' : 'application/json',
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            section: theSection,
+            event: theEvent
+
+        })
+    });
+    let the_data = await response.json();
+    console.log(the_data); 
+};
+
 
 async function add_event(category, eventName, the_name, the_date, the_location) {
     let response = await fetch("http://localhost:3000/add-event", {
@@ -36,7 +54,28 @@ async function add_event(category, eventName, the_name, the_date, the_location) 
     });
     let the_data = await response.json();
     console.log(the_data); 
-}
-add_event("the-weekend", "bison-day", "Comp Sci", "10-15-25", "Bailey");
-return_SmallEvent("the-weekend", "bison-day", 3);
+};
+//add_event("the-weekend", "bison-day", "Comp Sci", "10-15-25", "Bailey");
+//return_SmallEvent("the-weekend", "bison-day", 3);
+
+async function delete_event(category, eventName, the_name) {
+    let response = await fetch("http://localhost:3000/delete-event", {
+        method: "POST",
+        headers: {
+            'Accept' : 'application/json',
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            event_category : category,
+            event_name : eventName,
+            sub_event_name : the_name,
+
+        })
+    });
+    let the_data = await response.json();
+    console.log(the_data); 
+};
+delete_event("stem-day", "science", "Nursing");
+return_subEvent("stem-day", "science");
+
 
